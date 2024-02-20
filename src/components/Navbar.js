691f2d1fcf3ab6cfacae1b6a7df6fe28b47df769
Salma J <87875257+salmly@users.noreply.github.com>
2024-02-19 22:33:27 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,17 +68,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-
-            <Nav.Item>
-              <Nav.Link
-                href="https://salmly.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
           </Nav>
         </Navbar.Collapse>
       </Container>
